07caff2c834c71ca5e70dd5ba631a960687bf913
liuzhibin <1072095696@qq.com>
2022-03-21 10:58:07 +0800

    优化登录注册功能

--- Adoption.sol
+++ Adoption.sol
@@ -13,28 +13,23 @@ contract Adoption {
     
     //用户注册
     function register(address user) public returns(uint8) {
-        if (userMapping[user] > 0) { 
-            return userMapping[user]; 
-        }
+        require(userMapping[user] == 0,"user is registered");
         userIndex++;
         userMapping[user] = userIndex;
         return userIndex;
     }
     
     //判断用户是否可以登录（>0）
-    function login(address user) public view returns(uint8) { 
+    function login(address user) public view returns(uint8) {
+        require(userMapping[user] != 0,"user not register");
         return userMapping[user];
     }
     
     //领养宠物
-    function adopt(uint petId) public returns (uint) {
-    
+    function adopt(uint8 petId) public returns (uint8) {
         // 确保 id 在数组长度内
         require(petId >= 0 && petId <= 7);
-        uint userNotExist = 404;
-        if (userMapping[msg.sender] == 0) { 
-            return userNotExist;
-        }
+        require(userMapping[msg.sender] > 0);
         // 保存调用这地址
         adopters[petId] = msg.sender; 
         return petId;
